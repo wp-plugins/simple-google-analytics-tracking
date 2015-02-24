8159f6ad1c162539e62a22916049f55ab5762fe1
dustyn <dustyn@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-24 19:07:50 +0000

    Add 1.2 tag release
    git-svn-id: https://plugins.svn.wordpress.org/simple-google-analytics-tracking/tags/1.2@1098393 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/simple-ga-tracking-input.php
@@ -0,0 +1,63 @@
+<?php
+// Add input box under Settings menu for Google Analytics Tracking ID
+
+add_action( 'admin_menu', 'sgat_add_option' );
+
+function sgat_add_option() {
+	add_options_page( 'Google Analtics', 'Google Analytics', 'manage_options', 'simple-google-analytics-tracking.php', 'sgat_admin_input' );
+}
+
+function sgat_admin_input() {
+	?>
+	<div class="wrap">
+		<h2>Simple Google Analytics</h2>
+		
+		<form method="post" action="options.php">
+			<?php settings_fields( 'sgat_settings' ); ?>
+			<?php do_settings_sections( 'sgat_section' ); ?>
+			<?php submit_button( 'Save Tracking ID' ); ?>
+		</form>
+
+	</div>
+	<?php
+}
+
+add_action( 'admin_init', 'sgat_admin_init' );
+
+function sgat_admin_init() {
+	register_setting( 
+		'sgat_settings', // Settings Group Name
+		'sgat_tracking_code', // Settings Array Name
+		'sgat_input_sanitize' // Sanitize Function Name
+	);
+	add_settings_section(
+		'sgat_main_section', // Section ID
+		'Simple Google Analytics Settings', // Section Title
+		'sgat_section_text', // Section function name
+		'sgat_section' // Page to display section
+	);
+}
+
+function sgat_section_text() {
+
+	$html = '';
+
+	$html .= '<p>Find your Tracking ID after logging into your profile by going to:<br />Admin > Property > Tracking Info > Tracking Code</p>';
+	$html .= '<p>Enter your Google Analytics Tracking ID in the input box below.<br />(Editors and Administrators are not tracked)</p>';
+	
+	$html .= '<fieldset>';
+	$html .= '<p>';
+	$html .= '<b>Tracking ID:</b> ';
+	$html .= '<input type="text" id="sgat_tracking_code_input" name="sgat_tracking_code" value="' . esc_attr( get_option('sgat_tracking_code') ) . '" />';
+	$html .= '</p>';
+
+	$html .= '</fieldset>';
+	echo $html;
+}
+
+function sgat_input_sanitize( $input ) {
+
+	$input = sanitize_text_field( $input );
+
+	return $input;
+}
---
+++ includes/simple-ga-tracking-output.php
@@ -0,0 +1,44 @@
+<?php
+// Add Google Analytics Tracking Code to page if anything is input
+
+add_action( 'wp_head', 'sgat_add_tracking', 1 );
+
+function sgat_add_tracking() {
+
+	$tracking_id = apply_filters( 'sgat_tracking_id', get_option("sgat_tracking_code") );
+	
+	if(
+		!empty( $tracking_id ) // There is a tracking code
+		&& apply_filters( 'sgat_output_ga_code', sgat_allow_tracking() )
+	) {
+	?>
+	<!-- BEGIN: Simple Google Analytics Tracking Code -->
+	<script>
+		(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+		ga('create', '<?php echo esc_attr( $tracking_id ); ?>', 'auto');
+		ga('send', 'pageview');
+
+	</script>
+	<!-- END: Simple Google Analytics Tracking Code -->
+	<?php
+
+	} // If there is a tracking id and user is allowed to be tracked
+
+}
+
+function sgat_allow_tracking() {
+
+	if (
+		current_user_can( 'administrator' ) // The current user is an admin
+		|| current_user_can( 'editor' ) // The current user is an editor
+	) {
+
+		return false;
+	}
+
+	return true;
+}
